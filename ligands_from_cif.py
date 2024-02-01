8794148f5196a8f3be22691bb34f6e0d5ee44191
aneesh on laptop <35152061+aneeshshakthy@users.noreply.github.com>
2024-02-01 18:28:39 -0500

    somewhat more functional

--- ligands_from_cif.py
+++ ligands_from_cif.py
@@ -2,13 +2,12 @@
 import numpy as np
 import biotite
 from rdkit import Chem
-from rdkit.Chem import Draw
+from rdkit.Chem import MolToSmiles
 from collections import Counter
 from PyAstronomy import pyasl
 an = pyasl.AtomicNo()
 import biotite.structure.io.pdbx as pdbx
-from io import BytesIO
-import PySimpleGUI as sg
+from collections import OrderedDict
 
 # takes stack and makes rdkit mol object
 def mol_from_stack (stack, bonds):
@@ -33,6 +32,7 @@ def mol_from_stack (stack, bonds):
         mol.AddBond(i1, i2, order)
     
     Chem.SanitizeMol(mol)
+    smile = MolToSmiles(mol)
     return mol
 
 def cif_to_mols(file_path):
@@ -49,9 +49,11 @@ def cif_to_mols(file_path):
 
     # i think this is inefficient and i can just make a np array w zeros atp
     boolList = []
+    important_list = []
     for atom in stack:
         if any(item == atom.res_name for item in list(cres.keys())) and atom.res_name != 'HOH':
             boolList.append(1)
+            important_list.append([atom.chain_id, atom.res_id, atom.res_name])
         else:
             boolList.append(0)
     # dunno why i did this
@@ -59,44 +61,33 @@ def cif_to_mols(file_path):
 
     # stack of only small molecules
     new_stack = stack[boolArray]
-    
-    counterdict = Counter(new_stack.chain_id)
-    ligand_list = list(counterdict.keys())
+
     
     # this is the current return, but will change when I figure out how exactly we want to populate the df
     mol_list = []
 
-    for chain in ligand_list:
-        new_slice = []
-        for atom in new_stack:
-            if str(atom.chain_id) == chain:
-                new_slice.append(1)
-            else:
-                new_slice.append (0)
-        # again this really should have been initialized as an np array but i love lists apparently
-        slice_array = np.array(new_slice, dtype = bool)
-        working_stack = new_stack[slice_array]
-        working_bonds = biotite.structure.connect_via_residue_names(working_stack)
-        mol_list.append(mol_from_stack(working_stack,working_bonds.as_array()))
-    # divide into each residue, record chain, and record unique res ids, return mol_list, create new function which takes cif and populates df
-    return mol_list
+    for chain in list(OrderedDict.fromkeys(l[0] for l in important_list)):
+        for res in list(OrderedDict.fromkeys(x[1] for x in important_list)):
+            new_slice = []
+            for atom in new_stack:
+                if atom.res_id == res and atom.chain_id == chain:
+                    new_slice.append(1)
+                else:
+                    new_slice.append(0)
+            slice_array = np.array(new_slice, dtype=bool)
+            working_stack = new_stack[slice_array]
+            if working_stack.shape != (0,):
+                resname = working_stack[0].res_name
+                working_bonds = biotite.structure.connect_via_residue_names(working_stack)
+                mol_list.append([resname, chain, res, mol_from_stack(working_stack, working_bonds.as_array())])
+    
+        return mol_list
 
-def image_to_data(im):
-    with BytesIO() as output:
-        im.save(output, format="PNG")
-        data = output.getvalue()
-    return data
 
 if __name__ == '__main__':
     print('hii')
-    file_path = "c:\\Users\\anees\\Downloads\\1oxr.cif" # insert file path here
+    file_path = "c:\\Users\\anees\\Downloads\\7x5p.cif" # insert file path here
     mols = cif_to_mols(file_path)
     # this is just drawing the mols bc that's what I can do rn, but obv will be replaced by loading into the appropriate df
-    for mol in mols:
-        data = image_to_data(Draw.MolToImage(mol, returnPNG=True))
-        layout=[
-            [sg.Image(data=data)],
-            [sg.Button('OK')],
-            ]
-        sg.Window('Title', layout).read(close=True)
+    print(mols)
 
