094370621b3558ca554aea69ec1ac333c5a35dd5
aneesh on laptop <35152061+aneeshshakthy@users.noreply.github.com>
2024-02-14 17:24:27 -0500

    it works (pending ph 7 protonation)

--- ligands_from_cif.py
+++ ligands_from_cif.py
@@ -8,6 +8,22 @@ from PyAstronomy import pyasl
 an = pyasl.AtomicNo()
 import biotite.structure.io.pdbx as pdbx
 from collections import OrderedDict
+import pandas as pd
+import os
+import tqdm as tqdm
+
+def add_formal_charges(m):
+    m.UpdatePropertyCache(strict=False)
+    for at in m.GetAtoms():
+        if at.GetAtomicNum() == 7 and at.GetExplicitValence()==4 and at.GetFormalCharge()==0:
+            at.SetFormalCharge(1)
+        if at.GetAtomicNum() == 8 and at.GetExplicitValence()==3 and at.GetFormalCharge()==0:
+            at.SetFormalCharge(1)
+        if at.GetAtomicNum() == 8 and at.GetExplicitValence()==1 and at.GetFormalCharge()==0:
+            at.SetFormalCharge(-1)
+        if at.GetAtomicNum() == 5 and at.GetExplicitValence()==4 and at.GetFormalCharge()==0:
+            at.SetFormalCharge(-1)
+
 
 # takes stack and makes rdkit mol object
 def mol_from_stack (stack, bonds):
@@ -30,10 +46,14 @@ def mol_from_stack (stack, bonds):
             5: Chem.BondType.SINGLE,
             6: Chem.BondType.DOUBLE}[bond[2]]
         mol.AddBond(i1, i2, order)
-    
+    # use openbabel to protonate everything at pH seven, THEN add formal charges
+    add_formal_charges(mol)
     Chem.SanitizeMol(mol)
     smile = MolToSmiles(mol)
-    return mol
+    return smile
+
+
+
 
 def cif_to_mols(file_path):
     # load file
@@ -42,52 +62,60 @@ def cif_to_mols(file_path):
     bonds = biotite.structure.connect_via_residue_names(stack)
 
     # nonpolymer chain info for parsing
-    nonpoly = pdbx_file.get('pdbx_nonpoly_scheme')
-
-    cres = Counter(nonpoly['mon_id'])
-    cres.keys()
-
-    # i think this is inefficient and i can just make a np array w zeros atp
-    boolList = []
-    important_list = []
-    for atom in stack:
-        if any(item == atom.res_name for item in list(cres.keys())) and atom.res_name != 'HOH':
-            boolList.append(1)
-            important_list.append([atom.chain_id, atom.res_id, atom.res_name])
-        else:
-            boolList.append(0)
-    # dunno why i did this
-    boolArray = np.array(boolList, dtype=bool)
-
-    # stack of only small molecules
-    new_stack = stack[boolArray]
-
+    if 'pdbx_nonpoly_scheme' in pdbx_file.keys():
+        nonpoly = pdbx_file.get('pdbx_nonpoly_scheme')
     
-    # this is the current return, but will change when I figure out how exactly we want to populate the df
-    mol_list = []
+       
+        cres = Counter(nonpoly['mon_id'])
 
-    for chain in list(OrderedDict.fromkeys(l[0] for l in important_list)):
-        for res in list(OrderedDict.fromkeys(x[1] for x in important_list)):
-            new_slice = []
-            for atom in new_stack:
-                if atom.res_id == res and atom.chain_id == chain:
-                    new_slice.append(1)
-                else:
-                    new_slice.append(0)
-            slice_array = np.array(new_slice, dtype=bool)
-            working_stack = new_stack[slice_array]
-            if working_stack.shape != (0,):
-                resname = working_stack[0].res_name
-                working_bonds = biotite.structure.connect_via_residue_names(working_stack)
-                mol_list.append([resname, chain, res, mol_from_stack(working_stack, working_bonds.as_array())])
-    
+        # i think this is inefficient and i can just make a np array w zeros atp
+        boolList = []
+        important_list = []
+        for atom in stack:
+            if any(item == atom.res_name for item in list(cres.keys())) and atom.res_name != 'HOH':
+                boolList.append(1)
+                important_list.append([atom.chain_id, atom.res_id, atom.res_name])
+            else:
+                boolList.append(0)
+        # dunno why i did this
+        boolArray = np.array(boolList, dtype=bool)
+
+        # stack of only small molecules
+        new_stack = stack[boolArray]
+
+        
+        # this is the current return, but will change when I figure out how exactly we want to populate the df
+        mol_list = []
+
+        for chain in list(OrderedDict.fromkeys(l[0] for l in important_list)):
+            for res in list(OrderedDict.fromkeys(x[1] for x in important_list)):
+                new_slice = []
+                for atom in new_stack:
+                    if atom.res_id == res and atom.chain_id == chain:
+                        new_slice.append(1)
+                    else:
+                        new_slice.append(0)
+                slice_array = np.array(new_slice, dtype=bool)
+                working_stack = new_stack[slice_array]
+                if working_stack.shape != (0,):
+                    resname = working_stack[0].res_name
+                    working_bonds = biotite.structure.connect_via_residue_names(working_stack)
+                    mol_list.append([list(pdbx_file.keys())[0][0], resname, chain, res, mol_from_stack(working_stack, working_bonds.as_array())])
+        
         return mol_list
+
 
 
 if __name__ == '__main__':
     print('hii')
-    file_path = "c:\\Users\\anees\\Downloads\\7x5p.cif" # insert file path here
-    mols = cif_to_mols(file_path)
-    # this is just drawing the mols bc that's what I can do rn, but obv will be replaced by loading into the appropriate df
-    print(mols)
-
+    dir_path = "C:\\Users\\anees\\Downloads\\randomfolder1" # insert file path here
+    mols = []
+    for x in os.listdir(dir_path):
+        path1 = os.path.join(dir_path, x)
+        singlelig = cif_to_mols(path1)
+        if singlelig != None:
+            for m in singlelig:
+                mols.append(m)
+            
+    df = pd.DataFrame(mols, columns = ['File', 'Res', 'Chain','Res_ID', 'smiles'])
+    print(df)
