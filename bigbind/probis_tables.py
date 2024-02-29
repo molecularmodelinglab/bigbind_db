061f89eecbae679bf0f73cb4e6f0080742b671cd
akshaygud1401 <156377316+akshaygud1401@users.noreply.github.com>
2024-02-29 13:00:12 -0500

    Update probis_tables.py

--- bigbind/probis_tables.py
+++ bigbind/probis_tables.py
@@ -21,11 +21,11 @@ def add_example_table(con):
     # be deleted
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS example")
-    cur.execute("CREATE TABLE example (id INTEGER PRIMARY KEY, pdb TEXT, chain TEXT, residues TEXT)")
+    cur.execute("CREATE TABLE example (id INTEGER PRIMARY KEY, pdbID TEXT, chainID TEXT, residues TEXT)")
     con.commit()
 
     # Create a DataFrame
-    df = pd.DataFrame(columns=['pdb', 'chain', 'residues'])
+    df = pd.DataFrame(columns=['pdbID', 'chainID', 'residues'])
 
     # Write the data to a sqlite table
     df.to_sql('example', con, if_exists='append', index=False)
@@ -41,7 +41,7 @@ def add_data_to_example(con, df, row):
     #probisdf = df
 
     #Add row by item
-    df2 = pd.DataFrame([row], columns=['pdb', 'chain', 'residues'])
+    df2 = pd.DataFrame([row], columns=['pdbID', 'chainID', 'residues'])
     df = pd.concat([df, df2]).reset_index(drop=True)
     
     # first find all the names that are already in the table
