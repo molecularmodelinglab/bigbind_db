a4079c839014fc005c5dec0d836e12cfb5fd9b89
Michael Brocidiacono <mixarcid@unc.edu>
2024-04-19 04:16:11 -0700

    fixed all this git mess smh

--- bigbind/run.py
+++ bigbind/run.py
@@ -2,14 +2,13 @@ from prefect import flow
 
 from bigbind.load_chembl import load_chembl
 from bigbind.db import create_tables
+from bigbind.tanimoto_matrix import load_tanimoto_matrix
 
-@flow
+# @flow
 def make_bigbind():
     create_tables()
-    chembl_future = load_chembl()
-
-    chembl_future.result()
-
+    load_chembl()
+    load_tanimoto_matrix()
 
 if __name__ == "__main__":
     make_bigbind()
